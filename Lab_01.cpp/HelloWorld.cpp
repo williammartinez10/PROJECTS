879d0b604daccdb2e1b7cab4651a5a46fdeb8e94
williammartinez10 <william.martinez10@upr.edu>
2021-02-03 12:00:50 -0400

    added cout with string containing name

--- Lab_01.cpp/HelloWorld.cpp
+++ Lab_01.cpp/HelloWorld.cpp
@@ -57,5 +57,6 @@ int main(){
     string myStr = "I am practicing how to save changes";
     int number = 2021;
     cout << myStr << ' ' << number << endl;
+    cout << "William is my name" << endl;
     return 0;
 }
